4cab65d2b69cea2aa7f8e6b9f07ae85ac1ef28a8
TrixieAM <ta.morales.bugged@gmail.com>
2025-03-18 15:55:45 +0800

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -8,6 +8,31 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./pages/Home";
 import Unauthorized from "./components/Unauthorized";
+import LearningAndDevelopment from "./pages/LearningAndDevelopment";
+import AllIcons from "./components/Page";
+import SettingsForm from "./SettingsForm";
+import ChildrenInfo from "./pages/ChildrenInfo";
+import VoluntaryWork from "./pages/Voluntary";
+import Eligibility from "./pages/Eligibility";
+import College from "./pages/College";
+import Vocational from "./pages/Vocational";
+import PDSFileSelector from "./pages/PDSfile";
+import WorkExperience from "./pages/WorkExperience";
+import OtherSkills from "./pages/OtheSkills";
+import PersonalTable from "./pages/PersonTable";
+import ViewAttendanceRecord from "./components/ViewAttendanceRecord";
+import DailyTimeRecord from "./components/DailyTimeRecord";
+import DailyTimeRecordFaculty from "./components/DailyTimeRecordFaculty";
+import AttendanceForm from "./components/AllAttendanceRecord";
+import AttendanceSearch from "./components/ViewAttendance";
+import AttendanceModule from "./components/AttendanceModule";
+import AttendanceModuleFaculty from "./components/AttendanceModuleFaculty";
+import OverallAttendancePage from "./components/OverallAttendance";
+import PDS1 from "./components/PDS1";
+import PDS2 from "./components/PDS2";
+import PDS3 from "./components/PDS3";
+import PDS4 from "./components/PDS4";
+import OfficialTimeForm from "./components/OfficialTimeForm";
 
 const drawerWidth = 280;
 
@@ -54,17 +79,235 @@ function App() {
           <Sidebar open={open} handleClick={handleClick} open2={open2} handleClickAttendance={handleClickAttendance} />
         )}
 
-        {/* Main Content */}
-        <Box component="main" sx={{ flexGrow: 1, bgcolor: "background.default", p: 3, marginLeft: !["/", "/login", "/register"].includes(location.pathname) ? `${drawerWidth}px` : 0 }}>
-          <Toolbar />
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<ProtectedRoute allowedRoles={["administrator", "superadmin"]}><Home /></ProtectedRoute>} />
-            <Route path="/unauthorized" element={<Unauthorized />} />
-          </Routes>
+          {/* Main Content */}
+          <Box component="main" sx={{ flexGrow: 1, bgcolor: "background.default", p: 3, marginLeft: `${drawerWidth}px` }}>
+            <Toolbar />
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/" element={<Login />} />
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/allicons" element={<AllIcons />} />
+              <Route
+                path="/childreninfo"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <ChildrenInfo />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/voluntarywork"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <VoluntaryWork />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/learningdev"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <LearningAndDevelopment />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/eligibility"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <Eligibility />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/college"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <College />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/vocational"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <Vocational />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/workexperience"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <WorkExperience />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/personalinfo"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <PersonalTable />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/otherskills"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <OtherSkills />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/settings"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <SettingsForm onUpdate={fetchSettings} />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/view_attendance"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <ViewAttendanceRecord />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/search_attendance"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <AttendanceSearch />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/daily_time_record"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <DailyTimeRecord />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/daily_time_record_faculty"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <DailyTimeRecordFaculty />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/attendance_form"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <AttendanceForm />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/attendance_module"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <AttendanceModule />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/attendance_module_faculty"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <AttendanceModuleFaculty />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/attendance_summary"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <OverallAttendancePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/official_time"
+                element={
+                  <ProtectedRoute allowedRoles={["administrator", "superadmin"]}>
+                    <OfficialTimeForm />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/pdsfile"
+                element={
+                  <ProtectedRoute allowedRoles={["staff", "administrator", "superadmin"]}>
+                    <PDSFileSelector />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/pds1"
+                element={
+                  <ProtectedRoute allowedRoles={["staff", "administrator", "superadmin"]}>
+                    <PDS1 />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/pds2"
+                element={
+                  <ProtectedRoute allowedRoles={["staff", "administrator", "superadmin"]}>
+                    <PDS2 />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/pds3"
+                element={
+                  <ProtectedRoute allowedRoles={["staff", "administrator", "superadmin"]}>
+                    <PDS3 />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/pds4"
+                element={
+                  <ProtectedRoute allowedRoles={["staff", "administrator", "superadmin"]}>
+                    <PDS4 />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/unauthorized" element={<Unauthorized />} />
+            </Routes>
+          </Box>
+
+          {/* Footer */}
+          <Box
+            component="footer"
+            sx={{
+              position: "fixed",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              zIndex: (theme) => theme.zIndex.drawer + 1,
+              bgcolor: settings.footer_color || "#ffffff",
+              color: "white",
+              textAlign: "center",
+              padding: "20px",
+            }}
+          >
+            <Typography variant="body1">{settings.footer_text || "Default Footer Text"}</Typography>
+          </Box>
         </Box>
-      </Box>
     </ThemeProvider>
   );
 }
--- Sidebar.jsx
+++ Sidebar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Drawer, List, ListItem, ListItemText, ListItemIcon, Collapse, Toolbar } from "@mui/material";
 import { Link } from "react-router-dom";
-import { Dashboard as DashboardIcon, ExpandMore, ExpandLess, House, ChildFriendlyRounded, BadgeRounded } from "@mui/icons-material";
+import {
+  Dashboard as DashboardIcon,
+  ExpandMore,
+  ExpandLess,
+  House,
+  ChildFriendlyRounded,
+  BadgeRounded,
+  School,
+  Streetview,
+  Psychology as PsychologyIcon,
+  SportsKabaddi,
+  FileCopy,
+  Settings as SettingsIcon,
+  Logout as LogoutIcon,
+} from "@mui/icons-material";
 
 const drawerWidth = 280;
 
-const Sidebar = ({ open, handleClick, open2, handleClickAttendance }) => {
+const Sidebar = ({ open, handleClick, open2, handleClickAttendance, handleLogout }) => {
   return (
     <Drawer
       className="no-print"
@@ -21,6 +35,7 @@ const Sidebar = ({ open, handleClick, open2, handleClickAttendance }) => {
     >
       <Toolbar />
       <List>
+        {/* Home */}
         <ListItem button component={Link} sx={{ color: "black" }} to="/home">
           <ListItemIcon>
             <House sx={{ fontSize: 29, marginLeft: "-6%" }} />
@@ -28,6 +43,7 @@ const Sidebar = ({ open, handleClick, open2, handleClickAttendance }) => {
           <ListItemText primary="Home" />
         </ListItem>
 
+        {/* Dashboards */}
         <ListItem button onClick={handleClick} sx={{ color: "black" }}>
           <ListItemIcon>
             <DashboardIcon />
@@ -44,9 +60,66 @@ const Sidebar = ({ open, handleClick, open2, handleClickAttendance }) => {
               </ListItemIcon>
               <ListItemText primary="Personal Information" />
             </ListItem>
+
+            <ListItem button component={Link} to="/childreninfo" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <ChildFriendlyRounded />
+              </ListItemIcon>
+              <ListItemText primary="Children Information" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/college" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <School />
+              </ListItemIcon>
+              <ListItemText primary="College Information" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/otherskills" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <ChildFriendlyRounded />
+              </ListItemIcon>
+              <ListItemText primary="Other Skills" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/workexperience" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <ChildFriendlyRounded />
+              </ListItemIcon>
+              <ListItemText primary="Work Experience" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/vocational" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <Streetview />
+              </ListItemIcon>
+              <ListItemText primary="Vocational" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/learningdev" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <PsychologyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Learning and Development" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/voluntarywork" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <SportsKabaddi />
+              </ListItemIcon>
+              <ListItemText primary="Voluntary Work" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/eligibility" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <BadgeRounded />
+              </ListItemIcon>
+              <ListItemText primary="Eligibility" />
+            </ListItem>
           </List>
         </Collapse>
 
+        {/* Attendance Records */}
         <ListItem button onClick={handleClickAttendance} sx={{ color: "black" }}>
           <ListItemIcon>
             <DashboardIcon />
@@ -63,10 +136,60 @@ const Sidebar = ({ open, handleClick, open2, handleClickAttendance }) => {
               </ListItemIcon>
               <ListItemText primary="View Attendance" />
             </ListItem>
-        
-        
+
+            <ListItem button component={Link} to="/search_attendance" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <BadgeRounded />
+              </ListItemIcon>
+              <ListItemText primary="Search Attendance" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/daily_time_record" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <BadgeRounded />
+              </ListItemIcon>
+              <ListItemText primary="Daily Time Record" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/attendance_summary" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <BadgeRounded />
+              </ListItemIcon>
+              <ListItemText primary="Attendance Summary" />
+            </ListItem>
+
+            <ListItem button component={Link} to="/official_time" sx={{ color: "black" }}>
+              <ListItemIcon sx={{ marginRight: "-1rem" }}>
+                <BadgeRounded />
+              </ListItemIcon>
+              <ListItemText primary="Official Time Form" />
+            </ListItem>
           </List>
         </Collapse>
+
+        {/* PDS Files */}
+        <ListItem button component={Link} sx={{ color: "black" }} to="/pdsfile">
+          <ListItemIcon>
+            <FileCopy />
+          </ListItemIcon>
+          <ListItemText primary="Datasheet Files" />
+        </ListItem>
+
+        {/* Settings */}
+        <ListItem button component={Link} sx={{ color: "black" }} to="/settings">
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItem>
+
+        {/* Logout */}
+        <ListItem button sx={{ cursor: "pointer" }} onClick={handleLogout}>
+          <ListItemIcon>
+            <LogoutIcon />
+          </ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItem>
       </List>
     </Drawer>
   );
